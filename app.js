ba26241296a523ad99d9046e5d01d2a5e1c0e52f
cjacks0413 <chjackson93@gmail.com>
2014-11-05 10:33:39 -0500

    fix declaration of public dir

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 /* basic setup */ 
 var express = require('express');
-var app = module.exports = express();
-var port    = 	process.env.PORT || 8080;
-var router = express.Router();
+var app = module.exports = express(); 
+var port         = 	process.env.PORT || 8080;
+var router       = express.Router();
 var static_pages = require('./app/static'); 
-/* routes: home page, collections, feeds */ 
+var user         = require('./models/user'); 
+var path 		 = require('path'); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs'); 
-
-// collection 
-router.get('/collections', function (req, res) {
-	res.send("Here are my collections!"); 
-});
-
-// feeds
-router.get('/feeds', function(req, res) {
-	res.send("Now let's visualize some feeds!");
-});
-
-app.use('/', router); 
 app.use(static_pages); 
+app.use(user);
 app.listen(port); 
 console.log("Listening on port " + port); 
