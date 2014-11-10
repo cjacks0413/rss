75690481dbfad62117046e2ac5bf13d2cc57082b
cjacks0413 <chjackson93@gmail.com>
2014-11-10 10:45:37 -0500

    adding temporary routing forvisualizations

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 /* basic setup */ 
 var express = require('express');
-<<<<<<< HEAD
-var app = module.exports = express(
-	express.static(__dirname + '/public'));
-var port    = 	process.env.PORT || 8080;
-var router = express.Router();
-=======
+var mysql = require('mysql'); 
+var connection = require('express-myconnection'); 
 var app = module.exports = express(); 
 var port         = 	process.env.PORT || 8080;
 var router       = express.Router();
->>>>>>> ba26241296a523ad99d9046e5d01d2a5e1c0e52f
 var static_pages = require('./app/static'); 
 var user         = require('./models/user'); 
 var path 		 = require('path'); 
-
+var viz			 = require('./app/visualizations'); 
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* db */ 
+
+app.use(
+	connection(mysql, {
+		host: 'localhost',
+		user: 'rss_admin',
+		password: '',
+		port: 3306, 
+		database: 'rss_dev' 
+	}, 'request')
+);
 
 app.set('view engine', 'ejs'); 
 app.use(static_pages); 
 app.use(user);
+app.use(viz); 
 app.listen(port); 
 console.log("Listening on port " + port); 
