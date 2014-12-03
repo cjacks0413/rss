b020ee14461d1424bd2620246125c0f9d36b0f31
cjacks0413 <chjackson93@gmail.com>
2014-12-03 00:28:37 -0500

    update database collection lookup

--- models/collection.js
+++ models/collection.js
@@ -6,25 +6,29 @@
 var express = require('express'); 
 var app = require('../app');
 var db = require('../app/db');
-
 var collections = express.Router(); 
 
    
 collections.get('/collections', function(req, res) {
-	var id = 1; // take from curent session 
-	db.connection.query('SELECT id, name FROM collections WHERE userid = ?', id, function(err, c) {
-		if (err) throw err;
-		console.log("found collection");
-		res.render('show_collections', { collections : c } ); 
-	})
+	if (req.session.user_id) {
+		res.locals.signed_in = true; 
+		var id = req.session.user_id; // take from curent session 
+		db.connection.query('SELECT id, name FROM collections WHERE userid = ?', id, function(err, c) {
+			if (err) throw err;
+			res.render('show_collections', { collections : c } ); 
+		})
+	} 
 }); 
 
 collections.get('/collection/:id', function(req, res) {
-	var id = req.params.id; 
-	db.connection.query('SELECT id, name FROM feeds WHERE collection_id = ?', id, function(err, f) {
-		if (err) throw err;
-		res.render('get_collection', { feeds : f, collection : id}); 
-	});
+	if (req.session.user_id) {
+		res.locals.signed_in = true; 
+		var id = req.params.id; 
+		db.connection.query('SELECT id, name FROM feeds WHERE collection_id = ?', id, function(err, f) {
+			if (err) throw err;
+			res.render('get_collection', { feeds : f, collection : id}); 
+		});
+	}
 }); 
 
 module.exports = collections; 
